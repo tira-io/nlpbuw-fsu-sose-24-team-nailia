c392a49e6330d29f903bf336d525990857eba97f
Nailia (Nellie) <nailia.mirzakhmedova@uni-weimar.de>
2024-04-29 09:51:25 +0200

    Update authorship_verification_submission.py

--- authorship-verification-submission/authorship_verification_submission.py
+++ authorship-verification-submission/authorship_verification_submission.py
@@ -9,9 +9,26 @@ from sklearn.metrics import accuracy_score
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
+
 def write_answers(dataset_path: Path, predicted: list) -> None:
     """ Writes the given answers to a file compliant with the datasets format """
     open(Path(dataset_path) / 'truth.jsonl', 'w').writelines([json.dumps(line)+"\n" for line in predicted])
+
+def train_LR():
+    tira = Client()
+
+    # Loading train data
+    text_train = tira.pd.inputs("nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
+    targets_train = tira.pd.truths("nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
+    vectorizer = TfidfVectorizer()
+
+    # Fit the vectorizer on the training data and transform the text data into TF-IDF vectors
+    X_train = vectorizer.fit_transform(text_train['text'])
+    y_train = targets_train['generated']
+    # Train model
+    classifier = LogisticRegression() # Logistic Classifier with well interpretable results
+    classifier.fit(X_train, y_train)
+    return classifier, vectorizer
 
 tira = Client()
 
@@ -23,24 +40,13 @@ targets_train = tira.pd.truths("nlpbuw-fsu-sose-24", "authorship-verification-tr
 text_validation = tira.pd.inputs("nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
 targets_validation = tira.pd.truths("nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
 
-# print(text_train)
-# print(targets_train)
 
-vectorizer = TfidfVectorizer()
-
-# Fit the vectorizer on the training data and transform the text data into TF-IDF vectors
-X_train = vectorizer.fit_transform(text_train['text'])
-
+classifier, vectorizer = train_LR() # Logistic Classifier with well interpretable results
 # Transform the validation/test data into TF-IDF vectors
 X_validation = vectorizer.transform(text_validation['text'])
 
 # Extract labels
-y_train = targets_train['generated']
 y_validation = targets_validation['generated']
-
-# Train model
-classifier = LogisticRegression() # Logistic Classifier with well interpretable results
-classifier.fit(X_train, y_train)
 
 # Predict on validation/test data
 y_pred = classifier.predict(X_validation)
@@ -49,14 +55,11 @@ y_pred = classifier.predict(X_validation)
 accuracy = accuracy_score(y_validation, y_pred)
 print("Accuracy:", accuracy)
 
-
 # Write predictions to file
 predicted = [{'id': id_, 'generated': pred} for id_, pred in zip(text_validation['id'], y_pred)]
 
-
 # Convert predicted list to DataFrame
 predicted_df = pd.DataFrame(predicted)
-print(predicted_df)
 # Save DataFrame to JSON file
 output_directory = get_output_directory(str(Path(__file__).parent))
 predicted_df.to_json(Path(output_directory) / "predictions.jsonl", orient="records", lines=True)
